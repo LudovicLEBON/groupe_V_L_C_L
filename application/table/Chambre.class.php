098564376303e9f80ed00bacb93b7055fb82a6d5
vincent <vinmart@outlook.fr>
2023-03-27 17:03:10 +0200

    modification des vues chambres et hotel vincent

--- application/table/Chambre.class.php
+++ application/table/Chambre.class.php
@@ -1,10 +1,20 @@
 <?php
+
 /**
 Classe créé par le générateur.
-*/
-class Chambre extends Table {
-	public function __construct() {
+ */
+class Chambre extends Table
+{
+	public function __construct()
+	{
 		parent::__construct("chambre", "cha_id");
 	}
+
+	public function selectCategorieHotel(): array
+	{
+		$sql = "select * from chambre, categorie, hotel,tarifer, standing 
+		 where cha_categorie=cat_id and cha_hotel=hot_id and tar_standing=sta_id and tar_categorie ";
+		$result = self::$link->query($sql);
+		return $result->fetchAll();
+	}
 }
-?>
