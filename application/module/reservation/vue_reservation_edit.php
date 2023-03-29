9036cd7c584de89cc55633817909a70d08defcdf
LudovicLEBON <ludovic2.lebon@hotmail.fr>
2023-03-29 16:37:06 +0200

    Ludovic

--- application/module/reservation/vue_reservation_edit.php
+++ application/module/reservation/vue_reservation_edit.php
@@ -1,99 +1,63 @@
-<<<<<<< HEAD
-    <form method="post" action="<?= hlien("reservation", "save") ?>">
-        <input type="hidden" name="res_id" id="res_id" value="<?= $id ?>" />
+<form method="post" action="<?= hlien("reservation", "save") ?>">
+    <input type="hidden" name="res_id" id="res_id" value="<?= $id ?>" />
 
-        <div class='form-group'>
-            <label for='res_date_creation'>Date_creation</label>
-            <input readonly id='res_date_creation' name='res_date_creation' type="datetime" size='50' value='<?= mhe($res_date_creation) ?>' class='form-control' />
-        </div>
-        <div class='form-group'>
-            <label for='res_date_maj'>Date_maj</label>
-            <input id='res_date_maj' name='res_date_maj' type='datetime' size='50' value='<?= date("Y-m-d H:i") ?>' class='form-control' />
-        </div>
-        <div class='form-group'>
-            <label for='res_date_debut_sejour'>Date_debut_sejour</label>
-            <input id='res_date_debut_sejour' name='res_date_debut_sejour' type='date' size='50' value='<?= mhe($res_date_debut_sejour) ?>' class='form-control' />
-        </div>
-        <div class='form-group'>
-            <label for='res_date_fin_sejour'>Date_fin_sejour</label>
-            <input id='res_date_fin_sejour' name='res_date_fin_sejour' type='date' size='50' value='<?= mhe($res_date_fin_sejour) ?>' class='form-control' />
-        </div>
-        <div class='form-group'>
-            <label for='res_client'>Client</label>
-            <select id='res_client' name='res_client' class='form-control'>
-                <?= Table::HTMLoptions("select * from client order by cli_login", "cli_id", "cli_login", $res_client) ?>
-            </select>
-        </div>
-        <div class='form-group'>
-            <label for='res_hotel'>Hotel</label>
-            <selectt id='res_hotel' name='res_hotel' class='form-control'>
-                <?= Table::HTMLoptions("select * from hotel order by hot_nom", "hot_id", "hot_nom", $res_hotel) ?>
-                </select>
+    <div class='form-group'>
+        <label for='res_date_creation'>Date_creation</label>
+        <input readonly id='res_date_creation' name='res_date_creation' type="datetime" size='50' value='<?= mhe($res_date_creation) ?>' class='form-control' />
+    </div>
+    <div class='form-group'>
+        <label for='res_date_maj'>Date_maj</label>
+        <input id='res_date_maj' name='res_date_maj' type='datetime' size='50' value='<?= date("Y-m-d H:i") ?>' class='form-control' />
+    </div>
+    <div class='form-group'>
+        <label for='res_date_debut_sejour'>Date_debut_sejour</label>
+        <input id='res_date_debut_sejour' name='res_date_debut_sejour' type='date' size='50' value='<?= mhe($res_date_debut_sejour) ?>' class='form-control' />
+    </div>
+    <div class='form-group'>
+        <label for='res_date_fin_sejour'>Date_fin_sejour</label>
+        <input id='res_date_fin_sejour' name='res_date_fin_sejour' type='date' size='50' value='<?= mhe($res_date_fin_sejour) ?>' class='form-control' />
+    </div>
+    <div class='form-group'>
+        <label for='res_client'>Client</label>
+        <select id='res_client' name='res_client' class='form-control'>
+            <?= Table::HTMLoptions("select * from client order by cli_login", "cli_id", "cli_login", $res_client) ?>
+        </select>
+    </div>
+    <div class='form-group'>
+        <label for='res_hotel'>Hotel</label>
+        <input readonly id='res_hotel' name='res_hotel' type="text" size="50" value="<?= $res_hotel ?>" class='form-control' />
 
-        </div>
-        <div class='form-group'>
-            <label for='res_chambre'>Chambre</label>
-            <select id='res_chambre' name='res_chambre' class='form-control'>
-                <?= Table::HTMLoptions("select * from chambre,hotel,reservation where cha_hotel=hot_id and hot_id=", "cli_id", "cli_login", $res_client) ?>
-            </select>
-        </div>
-        </div>
-        <div class='form-group'>
-            <label for='res_prix_total'>Prix_total</label>
-            <input id='res_prix_total' name='res_prix_total' type="floor" step="0.01" min="0.01" size='50' value='<?= mhe($res_prix_total) ?>' class='form-control' />
-        </div>
-        <div class='form-group'>
-            <label for='res_etat'>Etat</label>
-            <?php $etat = ["initialisée", "validée", "en attente", "annulée"]; ?>
-            <select id='res_etat' name='res_etat' size='50' value='<?= mhe($res_etat) ?>' class='form-control'>
-                <?php foreach ($etat as $key) {
-                    $selected = ($key === $res_etat) ? "selected" : ""; ?>
-                    <option value="<?= $key ?>" <?= $selected ?>><?= $key ?></option>
-                <?php } ?>
-            </select>
-        </div>
-        <input class="btn btn-success" type="submit" name="btSubmit" value="Enregistrer" />
-    </form>
-=======
-    <form method="post" action="<?=hlien("reservation","save")?>">
-		<input type="hidden" name="res_id" id="res_id" value="<?= $id ?>" />
-		
-                        <div class='form-group'>
-                            <label for='res_date_creation'>Date_creation</label>
-                            <input id='res_date_creation' name='res_date_creation' type='date' size='10' value='<?=mhe($res_date_creation)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_date_maj'>Date_maj</label>
-                            <input id='res_date_maj' name='res_date_maj' type='date' size='10' value='<?=mhe($res_date_maj)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_date_debut_sejour'>Date_debut_sejour</label>
-                            <input id='res_date_debut_sejour' name='res_date_debut_sejour' type='date' size='10' value='<?=mhe($res_date_debut_sejour)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_date_fin_sejour'>Date_fin_sejour</label>
-                            <input id='res_date_fin_sejour' name='res_date_fin_sejour' type='date' size='10' value='<?=mhe($res_date_fin_sejour)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_prix_total'>Prix_total</label>
-                            <input id='res_prix_total' name='res_prix_total' type='number' size='10' value='<?=mhe($res_prix_total)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_etat'>Etat</label>
-                            <input id='res_etat' name='res_etat' type='number' size='10' value='<?=mhe($res_etat)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_client'>Client</label>
-                            <input id='res_client' name='res_client' type='text' size='50' value='<?=mhe($res_client)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_hotel'>Hotel</label>
-                            <input id='res_hotel' name='res_hotel' type='text' size='50' value='<?=mhe($res_hotel)?>'  class='form-control' />
-                        </div>
-                        <div class='form-group'>
-                            <label for='res_chambre'>Chambre</label>
-                            <input id='res_chambre' name='res_chambre' type='text' size='50' value='<?=mhe($res_chambre)?>'  class='form-control' />
-                        </div>
-		<input class="btn btn-success" type="submit" name="btSubmit" value="Enregistrer" />
-	</form>              
->>>>>>> 3a32f06fcbe102b97533da1c66e8efa0a19d59fd
+    </div>
+    <div class='form-group'>
+        <label for='res_chambre'>Chambre</label>
+        <select id='res_chambre' name='res_chambre' class='form-control'>
+            <?= Table::HTMLoptions("select * from chambre,hotel 
+            where cha_hotel=hot_id and hot_id=" . $res_hotel . " ", "cli_id", "cli_login", $res_client) ?>
+        </select>
+    </div>
+    <?php
+    $data = Donneracces::HTML_checkbox("select * frome donneracces,services,reservation,hotel,prestation 
+            where don_service=ser_id and don_reservation=res_id and res_hotel=hot_id and pres_hotel=hot_id and hot_id=" . $res_hotel . " order by ser_libelle", "ser_id", "ser_libelle", $don_service, "don_quantite", "don_id");
+    foreach ($data as $ligne) {
+        extract($ligne); ?>
+        <li><a href="oeuvre_edit.php?id=<?= $oeu_id ?>"><?= $oeu_titre ?></a> - <a href="dissocier.php?idoeuvre=<?= $oeu_id ?>&idauteur=<?= $aut_id ?>">dissocier</a></li>
+    <?php
+    }
+    ?>
+    </div>
+    <div class='form-group'>
+        <label for='res_prix_total'>Prix_total</label>
+        <input id='res_prix_total' name='res_prix_total' type="floor" step="0.01" min="0.01" size='50' value='<?= mhe($res_prix_total) ?>' class='form-control' />
+    </div>
+    <div class='form-group'>
+        <label for='res_etat'>Etat</label>
+        <?php $etat = ["initialisée", "validée", "en attente", "annulée"]; ?>
+        <select id='res_etat' name='res_etat' class='form-control'>
+            <?php foreach ($etat as $key) {
+                $selected = ($key == $res_etat) ? "selected" : ""; ?>
+                <option value="<?= $key ?>" <?= $selected ?>><?= $key ?></option>
+            <?php } ?>
+        </select>
+    </div>
+    <input class="btn btn-success" type="submit" name="btSubmit" value="Enregistrer" />
+</form>
--- application/table/Donneracces.class.php
+++ application/table/Donneracces.class.php
@@ -6,5 +6,74 @@ class Donneracces extends Table {
 	public function __construct() {
 		parent::__construct("donneracces", "don_id");
 	}
+
+	static public function HTML_liQtt($sql, $pk, $label, $id)
+	{
+		$resultat = self::$link->query($sql);
+		$s = "";
+		foreach ($resultat as $tab) {
+			if ($tab[$pk] == $id)
+				$sel = " selected ";
+			else
+				$sel = "";
+
+			$s = $s . "<li $sel value='$tab[$pk]'>$tab[$label]</li>";
+		}
+		return $s;
+	}
+
+	static public function HTML_li_services($sql, $pk, $label, $id)
+	{
+		$resultat = self::$link->query($sql);
+		$s = "";
+		foreach ($resultat as $tab) {
+			if ($tab[$pk] == $id)
+				$sel = " selected ";
+			else
+				$sel = "";
+
+			$s = $s . "<li $sel value='$tab[$pk]'>$tab[$label]</li>";
+		}
+		return $s;
+	}
+
+	/**
+	 * fonction générique pour générer les balises Checkbox et input d'un champ d'une table
+	 *
+	 * @param string $sql requete sql
+	 * @param string $pk nom du champ pk primaire
+	 * @param string $label nom du champ à afficher dans la balise OPTION
+	 * @param integer $id valeur à préselectionner
+	 * @param string $label2 nom du champ à afficher dans la balise OPTION
+	 * @param integer $id2 valeur à préselectionner
+	 */
+	static public function HTML_checkbox($sql, $pk, $label, $id,$label2,$id2)
+	{
+		$resultat = self::$link->query($sql);
+		$s = "";
+		foreach ($resultat as $tab) {
+			if ($tab[$pk] == $id)
+				$sel = " checked ";
+			else
+				$sel = "";
+
+			$s = $s . "<input type='checkbox' name='$id' id='$id' $sel class='form-control' value='$tab[$pk]'><label for='$id'>$label</label><br><label for='$id2'>$label2</label><input type='number' name='$id2' id='$id2' class='form-control' value='$tab[$label2]'>";
+		}
+		return $s;
+	}
+
+	static public function HTMLli($sql, $pk, $label, $id)
+	{
+		$resultat = self::$link->query($sql);
+		$s = "";
+		foreach ($resultat as $tab) {
+			if ($tab[$pk] == $id)
+				$sel = " selected ";
+			else
+				$sel = "";
+
+			$s = $s . "<li $sel value='$tab[$pk]'>$tab[$label]</li>";
+		}
+		return $s;
+	}
 }
-?>
