a9bc548c7ce9bdba4a6a53d653e362a2c1dc804f
Cerrajouani <112086950+Cerrajouani@users.noreply.github.com>
2023-03-31 15:06:48 +0200

    Chakib

--- application/gabarit/inc_entete.php
+++ application/gabarit/inc_entete.php
@@ -1,6 +1,6 @@
 <div class="entete">
 	<div class="row">
-		<div class="col"><a href="index.php"><img src="_images/Vivehotel.png" class="logo"></a></div>
+		<div class="col"><a href="index.php"><img src="_images/vivehotel.png" class="logo"></a></div>
 		<div class="col">
 			<h1 class="display-4"><?= SITE_NOM ?></h1>
 			<p class="lead">Vive hôtel un groupe hôtelier de grand standing en France</p>
--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -1,1 +1,8 @@
-<h1>Bienvenue sur le site du groupe hôtelier ViveHotel</h1>
+<h2>Bienvenue sur le site du groupe hôtelier ViveHotel !</h2>
+<div>
+    <img src="_images/France.jpg">
+    <p><h6>30 hôtels répartis en France</h6><h6 style="color:midnightblue"></h6></p>
+
+    <img src="_images/services.jpg">
+    <p><h6>Des services pour tous les goûts</h6><h6 style="color:midnightblue"></h6></p>
+</div>
