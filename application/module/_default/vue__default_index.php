0b3545035107c867fd4eeb2d0faf92e3d4739f1e
Cerrajouani <112086950+Cerrajouani@users.noreply.github.com>
2023-04-04 18:23:07 +0200

    Chakib

--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -1,11 +1,30 @@
 <h2>Bienvenue sur le site du groupe hôtelier ViveHotel !</h2>
-<div>
-    <img src="_images/France.jpg">
-    <p><h6>30 hôtels répartis en France</h6><h6 style="color:midnightblue"></h6></p>
 
-    <p><img src="_images/services.jpg">
-    <h6>Des services pour tous les goûts</h6><h6 style="color:midnightblue"></h6></p>
+<p><img src="_images/France.jpg">
+<h6 style="color:midnightblue">30 hôtels répartis en France</h6>
+</p><br>
 
-    <p><img src="_images/room4.jpg">
-    <p><h6>Sentez-vous y comme chez vous !</h6><h6 style="color:midnightblue"></h6></p>
-</div>
+<p>
+    <!--Formulaire de réservation-->
+    <h4>Réservez ici</h4><br>
+    <forme method="post">
+        <p>
+            <label for="datearrivee">Date d'arrivée : </label>
+            <input type="date" id="datearrivee" name="datearrivee" required>
+        </p>
+        <p>
+            <label for="datedepart">Date de départ : </label>
+            <input type="date" id="datedepart" name="datedepart" required>
+        </p>
+        <p>
+            <input type="submit" name="btsubmit" value="Lancer la recherche">
+        </p>
+    </forme>
+</p><br>
+
+<p><img src="_images/services.jpg">
+<h6 style="color:midnightblue">Des services pour tous les goûts</h6>
+</p><br>
+<p><img src="_images/room4.jpg">
+<h6 style="color:midnightblue">Sentez-vous y comme chez vous !</h6>
+</p><br>
