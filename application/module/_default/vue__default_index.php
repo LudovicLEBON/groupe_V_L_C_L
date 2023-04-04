1c18c233fd6383f59e2692332b22f10dc5dea18c
Cerrajouani <112086950+Cerrajouani@users.noreply.github.com>
2023-04-04 14:17:23 +0200

    les erreurs de chakib

--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -3,6 +3,9 @@
     <img src="_images/France.jpg">
     <p><h6>30 hôtels répartis en France</h6><h6 style="color:midnightblue"></h6></p>
 
-    <img src="_images/services.jpg">
-    <p><h6>Des services pour tous les goûts</h6><h6 style="color:midnightblue"></h6></p>
+    <p><img src="_images/services.jpg">
+    <h6>Des services pour tous les goûts</h6><h6 style="color:midnightblue"></h6></p>
+
+    <p><img src="_images/room4.jpg">
+    <p><h6>Sentez-vous y comme chez vous !</h6><h6 style="color:midnightblue"></h6></p>
 </div>
