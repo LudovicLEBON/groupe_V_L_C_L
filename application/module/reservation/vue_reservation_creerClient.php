8e1169f1f423d089ce2b4745afdfc98b5876b457
LudovicLEBON <ludovic2.lebon@hotmail.fr>
2023-04-05 15:16:22 +0200

    maj reservation vue ludovic

--- application/module/reservation/vue_reservation_creerClient.php
+++ application/module/reservation/vue_reservation_creerClient.php
@@ -2,55 +2,44 @@
     <input type="hidden" name="res_id" id="res_id" value="<?= $id ?>" />
 
     <div class='form-group'>
-        <label for='res_date_creation'>Date_creation</label>
+        <label for='res_date_creation'>Date de creation</label>
         <input readonly id='res_date_creation' name='res_date_creation' type="datetime" size='50' value='<?= date("Y-m-d H:i") ?>' class='form-control' />
     </div>
     <div class='form-group'>
-        <label for='res_date_maj'>Date_maj</label>
+
         <input id='res_date_maj' name='res_date_maj' type='hidden' size='50' value='<?= date("Y-m-d H:i") ?>' class='form-control' />
     </div>
     <div class='form-group'>
-        <label for='res_date_debut_sejour'>Date_debut_sejour</label>
+        <label for='res_date_debut_sejour'>Date de debut de sejour</label>
         <input id='res_date_debut_sejour' name='res_date_debut_sejour' type='date' size='50' value='<?= mhe($res_date_debut_sejour) ?>' class='form-control' />
     </div>
     <div class='form-group'>
-        <label for='res_date_fin_sejour'>Date_fin_sejour</label>
+        <label for='res_date_fin_sejour'>Date de fin de sejour</label>
         <input id='res_date_fin_sejour' name='res_date_fin_sejour' type='date' size='50' value='<?= mhe($res_date_fin_sejour) ?>' class='form-control' />
     </div>
 
     <div class='form-group'>
         <label for='res_hotel'>Hotel</label>
-        <select readonly id='res_hotel' name='res_hotel' type="text" size="50" value="hotel <?= $res_hotel ?>" class='form-control' />
-        <?= Hotel::HTMLoptionsHotel(
-            "select * from hotel,standing 
+        <select id='res_hotel' name='res_hotel' type="text" class='form-control'>
+            <?= Hotel::HTMLoptionsHotel(
+                "select * from hotel,standing 
             where hot_standing=sta_id and hot_statut='ouvert'",
-            "hot_id",
-            "hot_nom",
-            "sta_libelle",
-            $res_hotel
-        ) ?>
-        </select>
-    </div>
-    <div class='form-group'>
-        <label for='res_chambre'>Chambre</label>
-        <select id='res_chambre' name='res_chambre' class='form-control'>
-            <?= Chambre::HTMLoptionsChamb(
-                "select * from chambre,hotel,categorie 
-            where cha_hotel=hot_id and cha_categorie=cat_id and hot_id=" . $res_hotel . " and cha_statut='libre' ",
-                "cha_id",
-                "cha_nom",
-                "cha_type_lit",
-                "cat_libelle",
-                $res_chambre
+                "hot_id",
+                "hot_nom",
+                "sta_libelle",
+                $res_hotel
             ) ?>
         </select>
     </div>
-
-    </div>
     <div class='form-group'>
-        <label for='res_prix_total'>Prix_total</label>
-        <input readonly id='res_prix_total' name='res_prix_total' type="floor" step="0.01" min="0.01" size='50' value='<?= mhe($res_prix_total) ?>' class='form-control' />
+        <label for='res_commende'>Listez vos critaires de la chambre (type de lit, taille, catégorie...)</label>
+        <textarea id='res_commende' name='res_commende' rows="3" class='form-control'>
+          <?= mhe($res_commende) ?>
+          
+        </textarea>
     </div>
+
+
 
     <input class="btn btn-success" type="submit" name="btSubmit" value="Enregistrer" />
 </form>
--- application/table/Reservation.class.php
+++ application/table/Reservation.class.php
@@ -1,9 +1,12 @@
 <?php
+
 /**
 Classe créé par le générateur.
-*/
-class Reservation extends Table {
-	public function __construct() {
+ */
+class Reservation extends Table
+{
+	public function __construct()
+	{
 		parent::__construct("reservation", "res_id");
 	}
 	public function selectReservation(): array
@@ -31,13 +34,11 @@ class Reservation extends Table {
 	static public function selectAllClient($cli_id): array
 	{
 		$sql = "select * from client,hotel,reservation
-		wher cli_id=:id and cli_id=res_client and res_hotel=hot_id order by res_date_debut_sejour";
+		where cli_id=:id and cli_id=res_client and res_hotel=hot_id order by res_date_debut_sejour";
 
 		$stmt = Table::$link->prepare($sql);
 		$stmt->bindValue(":id", $cli_id, PDO::PARAM_INT);
 		$stmt->execute();
 		return $stmt->fetchAll();
 	}
-
-
 }
