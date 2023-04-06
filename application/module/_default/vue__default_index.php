f24d736755c3ac76899e1c1f1022056192d9391e
Cerrajouani <112086950+Cerrajouani@users.noreply.github.com>
2023-04-06 16:35:58 +0200

    MàJ Chakib

--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -3,14 +3,14 @@
 <table>
     <td>
 <p><img src="_images/France.jpg">
-<h6 style="color:rgb(220, 188, 11)">30 hôtels répartis en France</h6>
+<h6 style="color:rgb(10, 90, 250)">30 hôtels répartis en France</h6>
 </p><br>
     </td>
 <p>
     <td>
     <!--Formulaire de réservation-->
     <form method="post">
-    <h4>Réservez ici : </h4><br>
+    <h4 style="color:springgreen">Réservez ici : </h4><br>
         <p>
             <label for="datearrivee">Date d'arrivée : </label>
             <input type="date" id="datearrivee" name="datearrivee" required>
@@ -22,6 +22,7 @@
         <p>
             <input type="submit" name="btsubmit" value="Lancer la recherche">
         </p>
+        <h6 style="color:rgb(270, 0, 75)">Remboursable jusqu'au jour d'arrivée, avant 18 heures.</h6>
     </form>
     </td>
     </table>
@@ -29,7 +30,14 @@
 
 <p><img src="_images/services.jpg">
 <h6 style="color:rgb(220, 188, 11)">Des services pour tous les goûts</h6>
-</p><br>
+</p>
+<li>Wi-Fi</li>
+<li>Piscine</li>
+<li>Jacouzzi</li>
+<li>Salle de sport/fitness</li>
+<li>Salle détente (TV, tables de jeux, etc...)</li>
+
+<br><br>
 <p><img src="_images/room4.jpg">
 <h6 style="color:rgb(220, 188, 11)">Sentez-vous y comme chez vous !</h6>
 </p><br>
