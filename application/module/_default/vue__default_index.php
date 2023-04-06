87911be6f38c966592c222197d5cfb511454bb2a
Cerrajouani <112086950+Cerrajouani@users.noreply.github.com>
2023-04-06 17:03:17 +0200

    MaJ

--- application/module/_default/vue__default_index.php
+++ application/module/_default/vue__default_index.php
@@ -3,14 +3,14 @@
 <table>
     <td>
 <p><img src="_images/France.jpg">
-<h6 style="color:rgb(10, 90, 250)">30 hôtels répartis en France</h6>
+<h6 style="background-color:goldenrod">30 hôtels répartis en France</h6>
 </p><br>
     </td>
 <p>
     <td>
     <!--Formulaire de réservation-->
     <form method="post">
-    <h4 style="color:springgreen">Réservez ici : </h4><br>
+    <h4 style="color:goldenrod">Réservez ici : </h4><br>
         <p>
             <label for="datearrivee">Date d'arrivée : </label>
             <input type="date" id="datearrivee" name="datearrivee" required>
@@ -20,16 +20,16 @@
             <input type="date" id="datedepart" name="datedepart" required>
         </p>
         <p>
-            <input type="submit" name="btsubmit" value="Lancer la recherche">
+            <input style="background-color:goldenrod" type="submit" name="btsubmit" value="Lancer la recherche">
         </p>
-        <h6 style="color:rgb(270, 0, 75)">Remboursable jusqu'au jour d'arrivée, avant 18 heures.</h6>
+        <h6 style=color:goldenrod>Remboursable jusqu'au jour d'arrivée, avant 18 heures.</h6>
     </form>
     </td>
     </table>
 </p><br>
 
 <p><img src="_images/services.jpg">
-<h6 style="color:rgb(220, 188, 11)">Des services pour tous les goûts</h6>
+<h6 style="background-color:goldenrod">Des services pour tous les goûts</h6>
 </p>
 <li>Wi-Fi</li>
 <li>Piscine</li>
@@ -39,5 +39,5 @@
 
 <br><br>
 <p><img src="_images/room4.jpg">
-<h6 style="color:rgb(220, 188, 11)">Sentez-vous y comme chez vous !</h6>
+<h6 style="background-color:goldenrod">Sentez-vous y comme chez vous !</h6>
 </p><br>
